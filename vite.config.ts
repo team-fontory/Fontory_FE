01f206fe5f829ae42690781e2f4cdfd365fcd3a7
DongGyun <wooheuy5752@gmail.com>
2025-08-30 20:28:32 +0900

    chore: vite-plugin-svgr 설정 추가
    - svg 아이콘을 컴포넌트화 하기 위해 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
-});
+  plugins: [react(), tailwindcss(), tsconfigPaths(), svgr({ include: '**/*.svg?react' })],
+})
