bdf66858fa1d928aa8176a18b9082ee1fc7a2c79
DongGyun <wooheuy5752@gmail.com>
2025-09-06 12:31:31 +0900

    feat: 폰트 미리보기 섹션 컴포넌트 구현
    - 동적 폰트 textarea 구현
    - 폰트 미리보기 섹션 컴포넌트 구현

---
+++ src/domains/fonts/components/FontPreviewSection.tsx
@@ -0,0 +1,29 @@
+import type { FontDetail } from '../types/font.type'
+
+import { DynamicFontTextarea } from './DynamicFontTextarea'
+
+type Props = {
+  fontDetail: FontDetail
+}
+
+/** 폰트 미리보기 섹션 컴포넌트 */
+export const FontPreviewSection = ({ fontDetail }: Props) => {
+  return (
+    <section className='mt-20'>
+      <h4 className='text-accent-light text-2xl leading-8 font-bold'>미리보기</h4>
+
+      <div className='border-secondary mt-4 rounded-lg border bg-white p-6'>
+        <DynamicFontTextarea
+          fontName={fontDetail.fontName}
+          fontUrl={fontDetail.fontAddr}
+          rows={3}
+          className='text-accent w-full resize-none text-3xl leading-10 font-normal'
+          placeholder={fontDetail.example}
+        />
+        <p className='text-description text-lg font-normal'>
+          원하는 글자를 입력해 보며 글꼴을 테스트해 보세요.
+        </p>
+      </div>
+    </section>
+  )
+}
