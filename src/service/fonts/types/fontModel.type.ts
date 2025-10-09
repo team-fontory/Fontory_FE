67105c1d13dc172cb647e2272980809b52877686
DongGyun <wooheuy5752@gmail.com>
2025-10-09 18:28:05 +0900

    feat: 클라이언트에서 사용하는 폰트 타입 정의

---
+++ src/service/fonts/types/fontModel.type.ts
@@ -0,0 +1,32 @@
+import type { ProgressStatusValueType } from '@/shared/constants/progress.constant'
+
+/** 폰트 단일 항목 모델 */
+export type FontItemModel = {
+  fontId: number
+  fontName: string
+  writerName: string
+  example: string
+  isBookmarked: boolean
+  fontAddr: string
+  downloadCount: string
+  bookmarkCount: string
+}
+
+/** 폰트 목록 모델 */
+export type FontListModel = FontItemModel[]
+
+/** 제작 중인 폰트 단일 항목 모델 */
+export type InProgressFontModel = {
+  id: number
+  name: string
+  createdAt: string
+  formattedCreatedAt: string
+  status: string
+  statusText: ProgressStatusValueType
+}
+
+/** 제작 중인 폰트 목록 모델 */
+export type InProgressFontListModel = {
+  fontList: InProgressFontModel[]
+  count: number
+}
