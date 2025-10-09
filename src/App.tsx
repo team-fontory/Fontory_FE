dcde810bdeeba6738dac7b745150bf6736b54f74
DongGyun <wooheuy5752@gmail.com>
2025-10-09 18:25:32 +0900

    feat: 전역 로딩과 에러 처리 컴포넌트 구현 및 적용
    - AppRouter.tsx 제거 후, App.tsx에 직접 적용
    - PageLoader, PageError 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import { AppRouter } from './app/router/AppRouter'
+import { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import { routes } from './app/router/routes'
+import { PageError } from './presentation/components/shared/PageError'
+import { PageLoader } from './presentation/components/shared/PageLoader'
 
 function App() {
-  return <AppRouter />
+  return (
+    <ErrorBoundary
+      FallbackComponent={PageError}
+      onReset={() => window.location.reload()}
+    >
+      <Suspense fallback={<PageLoader />}>
+        <RouterProvider router={createBrowserRouter(routes)} />
+      </Suspense>
+    </ErrorBoundary>
+  )
 }
 
 export default App
