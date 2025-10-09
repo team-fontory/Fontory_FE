197e1579a049798a6d53806a7f861d59ed5f8778
DongGyun <wooheuy5752@gmail.com>
2025-10-09 19:03:57 +0900

    feat: PopularFontSection 파일 이동 및 데이터 적용

--- src/presentation/font-explore/components/PopularFontSection.tsx
+++ src/presentation/font-explore/components/PopularFontSection.tsx
@@ -1,20 +1,25 @@
 import { Link } from 'react-router-dom'
 
 import { createRoute } from '@/app/router/routes.constant'
+import { usePopularFontListViewModel } from '@/service/fonts/hooks/usePopularFontListViewModel'
 
-import { usePopularFontList } from '../services/useFontQuery'
-
+/** 인기 폰트 섹션 */
 export const PopularFontSection = () => {
-  const { data: popularFontListView } = usePopularFontList()
+  const popularFontList = usePopularFontListViewModel()
+
+  if (!popularFontList?.length) return null
 
   return (
     <section className='mt-8' aria-labelledby='popular-fonts-title'>
-      <h2 id='popular-fonts-title' className='px-4 py-5 text-2xl leading-8 font-bold'>
+      <h2
+        id='popular-fonts-title'
+        className='py-5 text-2xl leading-8 font-bold'
+      >
         인기 폰트
       </h2>
 
-      <div className='grid grid-cols-1 gap-8 p-4 md:grid-cols-3'>
-        {popularFontListView.list.map((font) => (
+      <div className='grid grid-cols-1 gap-8 py-4 md:grid-cols-3'>
+        {popularFontList.map((font) => (
           <Link
             key={font.fontId}
             to={createRoute.fontDetail(font.fontId)}
@@ -27,8 +32,12 @@ export const PopularFontSection = () => {
               {font.example}
             </div>
             <div>
-              <h3 className='text-accent text-base leading-7 font-bold'>{font.fontName}</h3>
-              <p className='text-description text-sm leading-5'>by {font.writerName}</p>
+              <h3 className='text-accent text-base leading-7 font-bold'>
+                {font.fontName}
+              </h3>
+              <p className='text-description text-sm leading-5'>
+                by {font.writerName}
+              </p>
               <div className='text-description mt-2 flex gap-3 text-xs'>
                 <span>다운로드 {font.downloadCount}</span>
                 <span>북마크 {font.bookmarkCount}</span>
